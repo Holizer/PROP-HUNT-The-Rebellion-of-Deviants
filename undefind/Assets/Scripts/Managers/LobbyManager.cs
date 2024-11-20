5d2aa10effcf1702f3450a13d3a0a328055bd97b
juju <126075349+Holizer@users.noreply.github.com>
2024-11-20 22:44:56 +0300

    v0.16+
    maxwell?

--- undefind/Assets/Scripts/Managers/LobbyManager.cs
+++ undefind/Assets/Scripts/Managers/LobbyManager.cs
@@ -170,7 +170,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
         {
             PhotonNetwork.CurrentRoom.IsOpen = false;
             PhotonNetwork.CurrentRoom.IsVisible = false;
-            PhotonNetwork.LoadLevel(Scene.GameMap2.ToString());
+            PhotonNetwork.LoadLevel(Scene.GameMap.ToString());
         }
     }
 
